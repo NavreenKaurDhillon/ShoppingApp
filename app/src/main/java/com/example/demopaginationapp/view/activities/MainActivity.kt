917cdae29897b31eee687689a5d0f474993582ee
ablyNavreen <navreen.kaur@fatbit.in>
2026-05-14 17:45:36 +0530

    xml code update

--- app/src/main/java/com/example/demopaginationapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/demopaginationapp/view/activities/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.demopaginationapp.view.activities
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.demopaginationapp.R
@@ -16,9 +19,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            // Apply the insets as padding to the view
+            // You can choose to apply only top, only bottom, or all
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,0)
+
+            insets
+        }
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
